de2182f28bcaeadc7b8ce829041ccf6035e2f997
szraksy <aksoysezer95@gmail.com>
2017-02-16 12:22:59 +0200

    first

---
+++ LAB1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WP_Lab_1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Blue;
+            textBox2.BackColor = Color.Green;
+            MessageBox.Show("You changed to Custom Still!");
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+            textBox2.BackColor = Color.Gray;
+            MessageBox.Show("You changed to Standard Still!");
+        }
+    }
+}
