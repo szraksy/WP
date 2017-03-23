bf9b48e11d2d7a3d24b9f7903b9b9ab54f458f17
szraksy <aksoysezer95@gmail.com>
2017-03-24 01:03:49 +0200

    Normal level uploading, Bitmap,Gradient and changes to the interface.

--- LAB3/Form1.Designer.cs
+++ LAB3/Form1.Designer.cs
@@ -34,11 +34,15 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(29, 12);
+            this.button1.Location = new System.Drawing.Point(12, 11);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 23);
             this.button1.TabIndex = 0;
@@ -48,7 +52,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(127, 12);
+            this.button2.Location = new System.Drawing.Point(98, 11);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -61,24 +65,24 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.Green;
-            this.label1.Location = new System.Drawing.Point(54, 299);
+            this.label1.Location = new System.Drawing.Point(76, 69);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 2;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(221, 11);
+            this.button3.Location = new System.Drawing.Point(179, 11);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.Size = new System.Drawing.Size(83, 23);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Draw Bezier Curves";
+            this.button3.Text = "Bezier Curves";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(362, 11);
+            this.button4.Location = new System.Drawing.Point(268, 10);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(103, 23);
             this.button4.TabIndex = 4;
@@ -88,19 +92,53 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(482, 11);
+            this.button5.Location = new System.Drawing.Point(553, 11);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(97, 23);
+            this.button5.Size = new System.Drawing.Size(80, 23);
             this.button5.TabIndex = 5;
             this.button5.Text = "Clear";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(604, 306);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(377, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Bitmap";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(455, 10);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Gradient";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 338);
+            this.ClientSize = new System.Drawing.Size(645, 403);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -108,8 +146,9 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Sezer Aksoy FAF-151";
+            this.Text = "Sezer Aksoy FAF-151 #lab3";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,6 +162,9 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- LAB3/Form1.cs
+++ LAB3/Form1.cs
@@ -12,98 +12,81 @@ namespace LAB3
 {
     public partial class Form1 : Form
     {
+        Graphics Draw;
         public Form1()
         {
             InitializeComponent();
+            Draw = pictureBox1.CreateGraphics();
         }
 
-       
 
 
-        public void DrawLinePoint(PaintEventArgs e)
-        {
-            Graphics g;
-            g = this.CreateGraphics();
-            // Create pen.
-            Pen blackPen = new Pen(Color.Black, 3);
 
-            // Create points that define line.
-            Point point1 = new Point(100, 100);
-            Point point2 = new Point(500, 100);
 
-            // Draw line to screen.
-            e.Graphics.DrawLine(blackPen, point1, point2);
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Visible = true;
-            Graphics g;
-            g = this.CreateGraphics();
-            g.Clear(this.BackColor);
-            System.Drawing.Graphics grafiknesne;
-            grafiknesne = this.CreateGraphics();
+
+            Draw.Clear(Color.White);
+
             //Rectangle
             Pen firca = new Pen(System.Drawing.Color.Red, 5);
-            grafiknesne.DrawRectangle(firca, 30, 40, 150, 50);
+            Draw.DrawRectangle(firca, 30, 40, 150, 50);
             //square
             Pen firca1 = new Pen(System.Drawing.Color.Blue, 5);
-            grafiknesne.DrawRectangle(firca1, 250, 150, 120, 120);
+            Draw.DrawRectangle(firca1, 250, 150, 120, 120);
             //ellipse
             Pen firca2 = new Pen(System.Drawing.Color.Black, 5);
-            grafiknesne.DrawEllipse(firca2, 250, 40, 150, 50);
+            Draw.DrawEllipse(firca2, 250, 40, 150, 50);
             //circle
             Pen firca3 = new Pen(System.Drawing.Color.Green, 5);
-            grafiknesne.DrawEllipse(firca3, 30, 150, 120, 120);
+            Draw.DrawEllipse(firca3, 30, 150, 120, 120);
 
-            label1.Text = "Drawing has been completed with success! You can check those 4 planes above.";
+            label1.Text = "Drawing has been completed with success! You can check those 4 planes down bellow.";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Graphics g;
-            g = this.CreateGraphics();
+            Draw.Clear(Color.White);
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             label1.Visible = true;
-            Graphics g;
-            g = this.CreateGraphics();
-            g.Clear(this.BackColor);
-            System.Drawing.Graphics grafiknesne;
-            grafiknesne = this.CreateGraphics();
+            Draw.Clear(Color.White);
             Pen firca = new Pen(System.Drawing.Color.Red, 5);
-            grafiknesne.DrawLine(firca, 40,40, 100, 100);
+            Draw.DrawLine(firca, 40, 40, 100, 100);
             Brush firca1 = new SolidBrush(System.Drawing.Color.Red);
             Font yazi = new System.Drawing.Font("Helvetica", 12, FontStyle.Italic);
-            grafiknesne.DrawString("Red Line", yazi, firca1, 5, 100);
-      
-           
+            Draw.DrawString("Red Line", yazi, firca1, 5, 100);
+
+
             Pen firca2 = new Pen(System.Drawing.Color.Blue, 5);
-            grafiknesne.DrawLine(firca2, 120,120, 170, 170);
+            Draw.DrawLine(firca2, 120, 120, 170, 170);
             Brush firca3 = new SolidBrush(System.Drawing.Color.Blue);
             Font yazi2 = new System.Drawing.Font("Helvetica", 12, FontStyle.Italic);
-            grafiknesne.DrawString("Blue Line", yazi2, firca3, 5, 150);
+            Draw.DrawString("Blue Line", yazi2, firca3, 5, 150);
 
             Pen firca4 = new Pen(System.Drawing.Color.Green, 5);
-            grafiknesne.DrawLine(firca4, 150, 200, 250, 200);
+            Draw.DrawLine(firca4, 150, 200, 250, 200);
             Brush firca5 = new SolidBrush(System.Drawing.Color.Green);
             Font yazi3 = new System.Drawing.Font("Helvetica", 12, FontStyle.Italic);
-            grafiknesne.DrawString("Green Line", yazi3, firca5, 5, 190);
+            Draw.DrawString("Green Line", yazi3, firca5, 5, 190);
 
             Pen firca6 = new Pen(System.Drawing.Color.HotPink, 5);
-            grafiknesne.DrawLine(firca6,170, 200, 230, 250);
+            Draw.DrawLine(firca6, 170, 200, 230, 250);
             Brush firca7 = new SolidBrush(System.Drawing.Color.HotPink);
             Font yazi4 = new System.Drawing.Font("Helvetica", 12, FontStyle.Italic);
-            grafiknesne.DrawString("Pink Line", yazi4, firca7, 5, 250);
+            Draw.DrawString("Pink Line", yazi4, firca7, 5, 250);
 
             Pen firca8 = new Pen(System.Drawing.Color.Purple, 5);
-            grafiknesne.DrawLine(firca8, 180, 230, 300, 300);
+            Draw.DrawLine(firca8, 180, 230, 300, 300);
             Brush firca9 = new SolidBrush(System.Drawing.Color.Purple);
             Font yazi5 = new System.Drawing.Font("Helvetica", 12, FontStyle.Italic);
-            grafiknesne.DrawString("Purple Line", yazi5, firca9, 5, 270);
-            label1.Text = "Drawing has been completed with success! You can check those 5 lines above.";
+            Draw.DrawString("Purple Line", yazi5, firca9, 5, 270);
+            label1.Text = "Drawing has been completed with success! You can check those 5 lines down bellow.";
 
 
         }
@@ -113,17 +96,17 @@ namespace LAB3
             label1.Visible = true;
             Pen kalemim = new Pen(Color.Red, 2);
             Pen kalemim2 = new Pen(Color.Blue, 2);
-            Graphics g;
 
-            
-            g = this.CreateGraphics();
-            g.Clear(this.BackColor);
+
+
+
+            Draw.Clear(Color.White);
             Point[] p1 = { new Point(100, 60), new Point(125, 100), new Point(150, 150), new Point(200, 50) };
-            g.DrawCurve(kalemim, p1, 1);
-      
+            Draw.DrawCurve(kalemim, p1, 1);
+
             Point[] p2 = { new Point(100, 120), new Point(125, 200), new Point(300, 250), new Point(200, 100) };
-            g.DrawCurve(kalemim2, p2, 2);
-            label1.Text = "Drawing has been completed with success! You can check those 2 bezier curves above.";
+            Draw.DrawCurve(kalemim2, p2, 2);
+            label1.Text = "Drawing has been completed with success! You can check those 2 bezier curves down bellow.";
 
 
         }
@@ -138,10 +121,97 @@ namespace LAB3
 
         private void button5_Click(object sender, EventArgs e)
         {
-            Graphics g;
-            g = this.CreateGraphics();
-            g.Clear(this.BackColor);
+            Draw.Clear(Color.White);
             label1.Visible = false;
+
+
+        }
+
+
+
+        private void pictureBox1_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Draw.Clear(Color.White);
+            }
+        }
+
+        public int xInit = 0, yInit = 0;
+        public int xFinal = 0, yFinal = 0;
+
+        Bitmap image1;
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+            //Red
+            Brush dolgu = new SolidBrush(System.Drawing.Color.Red);
+            //Yellow
+            Brush dolgu1 = new SolidBrush(System.Drawing.Color.Yellow);
+            //Black 
+            Brush dolgu2 = new SolidBrush(System.Drawing.Color.Black);
+            Draw.FillRectangle(dolgu, 20, 20, 200, 100);
+            Draw.FillRectangle(dolgu1, 20, 125, 50, 50);
+            Draw.FillEllipse(dolgu2, 20, 180, 100, 50);
+            Draw.FillEllipse(dolgu, 20, 240, 50, 50);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Retrieve the image.
+                image1 = new Bitmap(@"D:\FAF\2.Sınıf\2.Dönem\WP\LAB3\wp.bmp", true);
+
+                int x, y;
+
+                // Loop through the images pixels to reset color.
+                for (x = 0; x < image1.Width; x++)
+                {
+                    for (y = 0; y < image1.Height; y++)
+                    {
+                        Color pixelColor = image1.GetPixel(x, y);
+                        Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                        image1.SetPixel(x, y, newColor);
+                    }
+                }
+
+                // Set the PictureBox to display the image.
+                pictureBox1.Image = image1;
+
+                // Display the pixel format in Label1.
+                label1.Text = "Pixel format: " + image1.PixelFormat.ToString();
+
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+            }
+
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+            {
+                xFinal = e.X;
+                yFinal = e.Y;
+
+                Pen Bluepen = new Pen(Color.Blue, 5);
+                Pen Yellow = new Pen(Color.Yellow, 5);
+
+
+                Rectangle commonRect = new Rectangle(xInit, yInit, xFinal, yFinal);
+                
+
+                Draw.DrawEllipse(Bluepen, commonRect);
+                Point[] p1 = { new Point(100, 60), new Point(125, 100), new Point(150, 150), new Point(200, 50) };
+                Draw.DrawCurve(Yellow, p1, 1);
+
+            }
         }
     }
 }
